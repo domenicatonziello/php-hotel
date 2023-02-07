653164b621b967cb3b53c359aafda6523380f38e
domenicatonziello <domenica.tonziello10@gmail.com>
2023-02-07 15:58:25 +0100

    add table

--- index.php
+++ index.php
@@ -36,6 +36,13 @@
             'distance_to_center' => 50
         ],
     ];
+
+    $array_keys = ''; 
+    foreach ($hotels as $hotel){
+        $array_keys = array_keys ($hotel);
+        $array_keys = str_replace('distance_to_center', 'distance from center', $array_keys);
+    };
+
 ?>
 
 <!DOCTYPE html>
@@ -44,15 +51,30 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- bootstrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
     <title> PHP HOTEL </title>
 </head>
 <body>
-    <?php foreach ($hotels as $hotel) : ?>
-        <ul>
-            <?php foreach ($hotel as $key => $value) : ?>
-                <li> <strong> <?= $key ?> : </strong> <?= $value ?> </li>
-            <?php endforeach; ?>
-        </ul>
-    <?php endforeach; ?>
+    <div class="container mt-5">
+        <table class="table">
+            <thead>
+                <tr>
+                    <?php foreach ($array_keys as $key) : ?>
+                        <th scope="col"> <?= ucwords($key) ?> </th>
+                    <?php endforeach; ?>    
+                </tr>
+            </thead>
+            <tbody class="table-group-divider">
+                <?php foreach ($hotels as $hotel) : ?>
+                    <tr>
+                        <?php foreach ($hotel as $value) : ?>
+                            <td> <?= $value ?></td>
+                        <?php endforeach; ?>
+                    </tr>
+                <?php endforeach; ?>
+            </tbody>
+        </table>
+    </div>
 </body>
 </html>
